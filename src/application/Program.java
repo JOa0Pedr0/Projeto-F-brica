5b9b22619c4e5630fa1e7def4d3567adaa79a6f8
JP <guedesjoaopedro700@gmail.com>
2025-09-17 12:18:22 -0300

    Estrutura inicial do projeto com entidade Product e classe Principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,17 @@
 package application;
 
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		
+		Product parafuso = new Product(0.50,"Parafuso Sextavado", "Aço carbono 10mm");
+		
+		Product porca = new Product(0.75,"Porca Autotravante", "Aço inox 10mm");
 
+		System.out.println(parafuso);
+		System.out.println(porca);
 	}
 
 }
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -2,4 +2,55 @@ package entities;
 
 public class Product {
 
+	private int id;
+	private double precoCusto;
+	private String nome;
+	private String descricao;
+	private static int proximoId = 1;
+
+	public Product() {
+
+	}
+
+	public Product(double precoCusto, String nome, String descricao) {
+
+		this.precoCusto = precoCusto;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.id = proximoId++;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public double getPrecoCusto() {
+		return precoCusto;
+	}
+
+	public void setPrecoCusto(double precoCusto) {
+		this.precoCusto = precoCusto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDecricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", precoCusto=" + precoCusto + ", nome=" + nome + ", descricao=" + descricao + "]";
+	}
+	
+	
 }
